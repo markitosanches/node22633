ad9436382a62e55ab72f8d13677c257e078274c3
Marcos <markitosanches@gmail.com>
2023-05-01 14:30:24 -0400

    Seance 1

---
+++ node/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+app.get('/*', function (req, res) {
+    res.send('Salut')
+})
+
+const server = app.listen(8081, function() {
+    const host = server.address().address
+    const port = server.address().port
+
+    console.log("App listening ", host, port)
+})
