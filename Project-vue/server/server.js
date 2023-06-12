dec4064b37d05bbeaabcf978249199ddb93e3727
Marcos <markitosanches@gmail.com>
2023-06-12 15:00:11 -0400

    Seance 16 partie 2

---
+++ Project-vue/server/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const app = express()
+const corsOption = {
+    origin: 'http://localhost:8081'
+}
+
+app.use(cors(corsOption))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended:true}))
+
+//test
+app.get('/', (req, res) => {
+    res.json({message: 'Welcome'})
+})
+
+const PORT = 8080
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
